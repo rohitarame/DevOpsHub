53079ccef5ed066ec6e74797721d96376f5d8704
rohitarame <rohitarame5@gmail.com>
2026-07-12 12:09:37 +0530

    Update project

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -11,14 +11,3 @@ terraform {
       version = "~> 3.6"
     }
   }
-
-  # Remote state backend configuration.
-  # Replace the bucket name with your own globally unique S3 bucket.
-  backend "s3" {
-    bucket         = "devopshub-terraform-state-north"
-    key            = "prod/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "devopshub-terraform-locks"
-    encrypt        = true
-  }
-}
