83e40391bda72830da09b0ec76cd103101c9e6e0
rohitarame <rohitarame5@gmail.com>
2026-07-12 12:28:56 +0530

    Update project

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@
 # Create the S3 bucket and DynamoDB table before the first run.
 terraform {
   backend "s3" {
-    bucket         = "devopshub-terraform-state"
+    bucket         = "devopshub-terraform-state1
     key            = "prod/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-locks"
